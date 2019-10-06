06699bbff8f90f2081ebb8c98b5b6b52ac737ac8
Mike Cowgill <mike@algohub.com>
2019-10-05 17:08:42 -0700

    Added latest tag for docker build
    Former-commit-id: 9db48bbe19cef783e4515be77ecb6da0e76a5dc4

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///')
 
 docker build -t algohub/deployment-endpoint:$GIT_COMMIT .
+
+docker tag algohub/deployment-endpoint:$GIT_COMMIT algohub/deployment-endpoint:latest
